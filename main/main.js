cc67377ccb3c2ce3fabf9b00b28f07c58108211f
JessicaWh <1030918615@qq.com>
2018-04-19 18:40:44 +0800

    提交

--- main/main.js
+++ main/main.js
@@ -1,5 +1,21 @@
 // Write your cade below:
-function main() {};
+function main(sysNum , userNum) {
+
+    var countA = 0;
+    var countB = 0;
+    for(var val of sysNum){
+        if(userNum.indexOf(val) != -1 ){
+            if(userNum.indexOf(val) == sysNum.indexOf(val)){
+                countA++;
+            }
+            else {
+                countB++;
+            }
+        }
+    }
+
+    console.log(sysNum+'~'+userNum+':'+countA+'A'+countB+'B');
+};
 
 module.exports = {
     main
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,22 @@
 const main = require('../main/main');
 
 describe('main()', () => {
-
+    var sysNum = new Array;
+    var userNum ;
+    for (var i = 0; i < 9; i++) {
+        sysNum[i] = i + 1;
+    }
+    //排序
+    sysNum.sort(function() {
+       return 0.5 - Math.random();
+    });
+    sysNum.length =4;
+    var sysNumString = '';
+    for(var i=0 ; i<sysNum.length ; i++){
+        sysNumString += sysNum[i];
+    }
+    it('guess num',function () {
+        userNum = '2451';
+        main.main(sysNumString ,userNum);
+     });
 });
